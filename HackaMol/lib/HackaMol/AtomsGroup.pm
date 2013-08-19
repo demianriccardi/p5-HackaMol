5021c0a2c4d3718359e14389608d6a510ec49009
Demian Riccardi <demianriccardi@gmail.com>
2013-08-18 23:20:41 -0400

    added _build_mass method to Molecule

--- HackaMol/lib/HackaMol/AtomsGroup.pm
+++ HackaMol/lib/HackaMol/AtomsGroup.pm
@@ -6,36 +6,6 @@ use Carp;
 use MooseX::Storage;
 with Storage( 'io' => 'StorableFile' ),'AtomsGroupRole';
 
-has $_ => (
-            is  => 'rw'  ,
-            isa => 'Num' ,
-            default => 1 ,
-            lazy    => 1 ,
-            clearer => 'clear_bond_order',
-          ) foreach qw(bond_order);
-
-has $_ => (
-            is      => 'rw'  ,
-            isa     => 'Math::Vector::Real' ,
-            clearer => 'clear_bond_vector',
-            builder => '_build_bond_vector',
-            lazy    => 1,
-          ) foreach qw(bond_vector);
-
-sub _build_bond_vector{
-  my $self  = shift;
-  my @atoms = $self->all_atoms;
-  return ($atoms[0]->inter_dcoords($atoms[1]));
-}
-
-has $_ => (
-            is      => 'rw'  ,
-            isa     => 'Num' ,
-            clearer => 'clear_bond_length',
-            builder => '_build_bond_length',
-            lazy    => 1,
-          ) foreach qw(bond_length);
-
 sub Rg {
  #radius of gyration. 
     my $self         = shift;
@@ -50,16 +20,16 @@ sub Rg {
     return( sqrt($sum/$total_mass) );
 }
 
-sub _clear_group_stuff => sub {
+sub _clear_group_attrs {
     my $self = shift;
-    foreach my $clearthis (qw(
-                              clear_bond_length
-                              clear_bond_vector
-                              clear_bond_order
+    foreach my $clearthis (qw(clear_dipole clear_COM clear_COZ
+                              clear_dipole_moment clear_total_charge
+                              clear_total_mass clear_total_Z 
+                              clear_atoms_bin
                              )){
     $self->$clearthis;
     }
-};
+}
 
 
 __PACKAGE__->meta->make_immutable;
--- HackaMol/lib/HackaMol/Molecule.pm
+++ HackaMol/lib/HackaMol/Molecule.pm
@@ -3,8 +3,39 @@ package Molecule;
 use Moose;
 use lib 'lib/roles';
 use MooseX::Storage;
-with Storage('io' => 'StorableFile'), 'PhysVecMVRRole';
+with Storage('io' => 'StorableFile'), 'PhysVecMVRRole',
+'BondsAnglesDihedralsRole';
 
+has 'atomgroups'  => (
+    traits   => ['Array'],
+    isa      => 'ArrayRef[AtomsGroup]',
+    default  => sub { [] },
+    lazy     => 1,
+    handles  => {
+        "has_atomgroups"   => 'count',
+        "push_atomgroups"   => 'push',
+        "get_atomgroups"   => 'get',
+        "set_atomgroups"   => 'set',
+        "all_atomgroups"   => 'elements',
+        "count_atomgroups" => 'count',
+        "break_atomgroups" => 'delete',
+        "clear_atomgroups" => 'clear',
+    },
+);
+
+sub atoms {
+  my $self = shift;
+  my @atoms;
+  push @atoms,$_->all_atoms  foreach $self->all_atomgroups;
+  return @atoms;
+}
+
+sub _build_mass {
+  my $self = shift;
+  my $mass = 0;
+  $mass += $_->mass foreach $self->atoms;
+  return ($mass); 
+}
 
 1;
 

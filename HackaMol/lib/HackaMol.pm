787afbf8bb0c7e7de0dfbf1724b5c124613c3b1c
Demian Riccardi <dde@ornl.gov>
2013-08-20 13:39:52 -0400

    initiated the HackaMol.pm  to wrap the core
    is this needed?  also deprecated a few roles

--- HackaMol/lib/HackaMol.pm
+++ HackaMol/lib/HackaMol.pm
@@ -1,11 +1,15 @@
 package HackaMol;
 # ABSTRACT: object-oriented library for multi-scale molecular hacking 
 use lib 'lib';
-#use HackaMol::Atom;
-#use HackaMol::Molecule;
-#use HackaMol::Bond;
-#use HackaMol::Angle;
-#use HackaMol::Dihedral;
+use Math::Vector::Real qw(V);
+require Exporter;
+our @ISA=qw(Exporter);
+our @EXPORT = qw(V);
+use HackaMol::Atom;
+use HackaMol::Molecule;
+use HackaMol::Bond;
+use HackaMol::Angle;
+use HackaMol::Dihedral;
 
 
 1;

39f78fd3765db8b9dc515c3f463a74ab44b73d4e
Demian Riccardi <dde@ornl.gov>
2013-08-22 16:49:23 -0400

    AtomGroupRole POD

--- HackaMol/lib/roles/AtomGroupRole.pm
+++ HackaMol/lib/roles/AtomGroupRole.pm
@@ -175,258 +175,132 @@ $_->push_charges(0.417) foreach ($atom1, $atom2);
 
 my $group = Class_with_AtomGroupRole->new(atoms=> [$atom1,$atom2,$atom3]);
 
+print $group->count_atoms . "\n"; #3
 
-print $group->count_atoms . "\n";
+print $group->total_charge . "\n"; # 0
+
+print $group->total_mass . "\n";  
 
 my @atoms = $group->all_atoms;
+
+print $group->dipole_moment . "\n";
 
 $group->do_forall('push_charges',0);
 
 $group->do_forall('push_coords',$group->COM);
 
-print $group->dipole_moment . "\n";
-
 $group->gt(1); # same as $group->do_forall('t',1);
 
 print $group->dipole_moment . "\n";
 
+print $group->canonical_name . "\n";
 
-
+print $group->unique_atoms . "\n";
 
 =head1 DESCRIPTION
 
-PhysVecMVR provides the core attributes and methods shared between Atom 
-and Molecule classes. Consuming this role gives Classes a place to store 
-coordinates, forces, and charges, perhaps, over the course of a simulation 
-or for a collection of configurations for which all other object metadata (name, 
-mass, etc) remains fixed. As such, the 't' attribute, described below, is 
-important to understand. The PhysVecMVR uses Math::Vector::Real (referred to as MVR), 
-which has pure Perl and XS implementations.  MVR::XS is fast with many useful/powerful
-overloaded methods. PhysVecMVR leaves many attributes rw so that they may be set and 
-reset on the fly. This seems most intuitive from the 
-perspective of carrying out computational work on molecules.  Thus, HackaMol bravely 
-ignores Moose recommendations to use mostly 'ro' attributes and to generate 
-objects as needed.  HackaMol may be coerced to be more rigid in future releases.    
+The HackaMol AtomGroupRole class provides core methods and attributes for 
+consuming classes that use groups of atoms. The original implementation of 
+this role relied heavily on attributes, builders, and clearers.  Such an approach
+naturally gives fast lookup tables, but the ability to change atoms and coordinates
+made the role to difficult.  Such an approach may be pursued again (without changing
+the API) in the future after the API has matured.  The AtomGroupRole calculates all
+values for atoms using their own t attributes.
 
-Comparing the PhysVec within Atom and Molecule may be helpful. For both, the PhysVecRol 
-generates a little metadata (mass, name, etc.) and an array of coordinates, forces, and 
-charges.  For an atom, the array of coordinates gives an atom (with fixed metadata) the ability 
-to store multiple [x,y,z] positions (as a function of time, symmetry, distribution, etc.). What 
-is the array of coordinates for Molecule? Usually, the coordinates for a molecule will likely 
-remain empty (because the atoms that Molecule contains have the more useful coordinates), but we
-can imagine using the coordinates array to track the center of mass of the molecule if needed. 
+=array_method push_atoms, get_atoms, set_atoms, all_atoms, count_atoms, clear_atoms
 
-In the following:  Methods with mean_foo msd_foo intra_dfoo out front, carries out some analysis
-within $self. Methods with inter_ out front carries out some analysis between
-two objects that "does" PhysVecMVR (tests for this should be added) at the
-$self->t and $obj->t; a warning is carped if the ts are different 
+ARRAY traits for the atoms attribute, respectively: push, get, set, elements, count, clear
 
-=array_method push_$_, all_$_, get_$_, set_$_, count_$_, clear_$_ foreach qw(charges coords forces)
+=array_method push_atoms
 
-  ARRAY traits, respectively: push, get, set, all, elements, clear
-  Descriptions for charges and coords follows.  forces analogous to coords.
+push atom on to atoms array
+
+$group->push_atoms($atom1, $atom2, @otheratoms);
+
+=array_method all_atoms
+
+returns array of all elements in atoms array
+
+print $_->symbol, "\n" foreach $group->all_atoms; 
+
+=array_method get_atoms
+
+return element by index from atoms array
+
+print $group->get_atoms(1); # returns $atom2 from above
+
+=array_method set_atoms
+
+set atoms array by index
+
+$group->set_atoms(1, $atom1);
+
+=array_method count_atoms
+
+return number of atoms in group 
   
-=array_method push_charges
+print $group->count_atoms; 
 
-  push value on to charges array
+=array_method clear_atoms
 
-  $obj->push_charges($_) foreach (0.15, 0.17, 0.14, 0.13);
+clears atoms array
 
-=array_method all_charges
+=method do_for_all
 
-  returns array of all elements in charges array
+pass method and arguments down to atoms in group
 
-    print $_ . " " foreach $obj->all_charges; # prints 0.15 0.17 0.14 0.13
+$group->do_for_all('t',1); #sets t to 1 for all atoms
 
-=array_method get_charges
-
-  return element by index from charges array
-
-    print $obj->get_charges(1); # prints 0.17
-
-=array_method set_charges
-
-  set value of element by index from charges array
-
-    $obj->set_charges(2, 1.00);
-    print $_ . " " foreach $obj->all_charges; # prints 0.15 0.17 1.00 0.13
-
-=array_method count_charges
-
-  return number of elements in charges array
+=method gt
   
-    print $obj->count_charges; # prints 4 
+integer argument. wraps do_for_all for setting time within group
 
-=array_method clear_charges
-  
-  clears charges array
-    
-    $obj->clear_charges;
-    print $_ . " " foreach $obj->all_charges; # does nothing 
-    print $obj->count_charges; # prints 0
+$group->gt(1);
 
-=array_method push_coords
+=method dipole
 
-  push value on to coords array
+no arguments. return dipole calculated from charges and coordinates as Math::Vector::Real object  
 
-  $obj->push_coords($_) foreach ([0,0,0],[1,1,1],[-1.0,2.0,-4.0], [3,3,3]);
+=method COM
 
-=array_method all_coords
+no arguments. return center of mass calculated from masses and coordinates as Math::Vector::Real object  
+ 
+=method COM
 
-  returns array of all elements in coords array
+no arguments. return center of nuclear charge calculated from Zs and coordinates as Math::Vector::Real object  
 
-    printf ("%10.3f %10.3f %10.3f \n", @{$_}) foreach $obj->all_coords; 
+=method total_charge
 
-    my @new_coords = map {[$_->[0]+1,$_->[1]+1,$_->[2]+1]} $obj->all_coords;
+no arguments. return sum of atom charges.
 
-    printf ("%10.3f %10.3f %10.3f \n", @{$_}) foreach @new_coords; 
+=method total_mass
 
-=array_method get_coords
+no arguments. return sum of atom masses.
 
-  return element by index from coords array
+=method total_Z
 
-    printf ("%10.3f %10.3f %10.3f \n", @{$obj->get_coords(1)}); # 
+no arguments. return sum of Zs.
 
-=array_method set_coords
+=method dipole_moment
 
-  set value of element by index from coords array
+no arguments. returns the norm of the dipole in debye (assuming charges in electrons, AKMA)
 
-    $obj->set_coords(2, [100,100,100]);
-    printf ("%10.3f %10.3f %10.3f \n", @{$_}) foreach $obj->all_coords; 
+=method bin_atoms
 
-=array_method count_coords
+no arguments. returns two hash references. The histogram of atom symbols, and a map from symbol-> Z for the same
+keys.  The second hash reference was added, to be able to sort by Z in the absence of Atom objects.
 
-  return number of elements in coords array
-  
-    print $obj->count_coords; # prints 4 
+=method count_unique_atoms
 
-=array_method clear_coords
-  
-  clears coords array
-    
-    $obj->clear_coords;
-    print $_ . " " foreach $obj->all_coords; # does nothing 
-    print $obj->count_coords # prints 0
+no arguments. returns the number of keys in each hash returned by bin_atoms
 
-=attr name
+=method canonical_name
 
-isa Str that is rw. useful for labeling, bookkeeping...
+no arguments. returns a string summary of the atoms in the group.  Take the bin_atoms hashes, sorts
+by Z and generates something like OH2 for water or O2H2 for peroxide.
 
-=attr t 
+=attr atoms
 
-isa Int or ScalarRef that is rw with default of 0
+isa ArrayRef[Atom] that is lazy with public ARRAY traits described in ARRAY_METHODS
 
-t is intended to describe the current "setting" of the object. Objects that 
-consume the PhysVecRol have  arrays of coordinates, forces, and charges to 
-allow storage with the passing of time (hence t) or the generation alternative 
-configurations.  For example, a crystal lattice can be stored as a single 
-object that consumes PhysVec (with associated metadata) along with the 
-array of 3d-coordinates resulting from lattice vector translations. 
-
-Experimental: Setting t to a ScalarRef allows all objects to share the same t.  
-Although, to use this, a $self->t accessor that dereferences the value would 
-seem to be required.  There is nothing, currently, in the core to do so. Not 
-sure yet if it is a good or bad idea to do so.
-
-    my $t  = 0;
-    my $rt = \$t;  
-    $_->t($rt) for (@objects);
-    $t = 1; # change t for all objects.
-
-=attr mass 
-
-isa Num that is rw and lazy with a default of 0
-
-=attr xyzfree
-
-isa ArrayRef that is rw and lazy with a default value [1,1,1]. Using this array
-allows the object to be fixed for calculations that support it.  For example, to
-fix the X and Y coordinates:
-
-  $obj->xyzfree([0,0,1]);
-
-fixing any coordinate will set trigger the is_fixed(1) flag.  
-
-=attr is_fixed
-
-isa Bool that is rw and lazy with a default of 0 (false).
-
-=attr charges
-
-isa ArrayRef[Num] that is lazy with public ARRAY traits described in ARRAY_METHODS
-
-Gives atoms and molecules t-dependent arrays of charges. e.g. store and analyze
-atomic charges from a quantum mechanical molecule in several intramolecular 
-configurations or a fixed configuration in varied external potentials. 
-
-=attr coords forces
-
-isa ArrayRef[Math::Vector::Real] that is lazy with public ARRAY traits described in ARRAY_METHODS
-
-Gives atoms and molecules t-dependent arrays of coordinates and forces,
-for the purpose of analysis.  
-
-=method distance
-
-Takes one argument ($obj2) and calculates the distance using Math::Vector::Real
-
-  $obj1->distance($obj2);
-
-=method angle
-
-Takes two arguments ($obj2,$obj3) and calculates the angle (degrees) between 
-the vectors with $obj1 as orgin using Math::Vector::Real.  
-
-  $obj1->angle($obj2,$obj3);
-
-=method intra_dcharges
-
-Calculates the change in charge from initial t ($ti) to final t ($tf). I.e.:
-
-$self->get_charges($tf) - $self->get_charges($ti)
-
-=method mean_charges
-
-No arguments.  Calculates the mean of all stored charges.
-
-=method msd_charges
-
-No arguments.  Calculates the mean square deviation of all stored charges.
-
-=method intra_dcoords intra_dforces 
-
-returns the difference (Math::Vector::Real object) from the initial t ($ti) to
-the final t ($tf).
-
-$obj1->intra_dcoords($ti,$tf);
-
-$obj1->intra_dforces($ti,$tf);
-
-=method mean_coords mean_forces 
-
-No arguments. Calculates the mean (Math::Vector::Real object) vector.
-
-=method msd_coords msd_forces 
-
-No arguments. Calculates the mean (Math::Vector::Real object) dot product of the
-vector difference of the xyz coords from the mean vector.
-
-=method charge
-
-called with no arguments.  returns $self->get_charges($self->t);
-
-=method xyz
-
-called with no arguments.  returns $self->get_coords($self->t);
-
-=method force
-
-called with no arguments.  returns $self->get_forces($self->t);
-
-=method copy_ref_from_t1_through_t2
-
-called as $obj->copy_ref_from_t1_through_t2($qcf,$t,$tf); where qcf is
-charges|coords|forces.  Fills the qcf from t+1 to tf with the value at t.
-Added this while trying to compare a dipole as a function of changing charges
-at a single set of coordinates.
 

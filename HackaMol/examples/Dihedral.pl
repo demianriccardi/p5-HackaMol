dc57cd61bb4542abda66d7f4de24d14a5a6b46ac
Demian Riccardi <demianriccardi@gmail.com>
2013-08-21 22:42:32 -0400

    Improve examples/Dihedral.pl a little faster

--- HackaMol/examples/Dihedral.pl
+++ HackaMol/examples/Dihedral.pl
@@ -6,15 +6,19 @@ use PDBintoAtoms qw(readinto_atoms);
 use Time::HiRes qw(time);
 
 my $t1 = time; 
+#my @atoms = readinto_atoms("t/lib/2CBA.pdb");
 my @atoms = readinto_atoms("t/lib/1L2Y.pdb");
 my $max_t = $atoms[0]->count_coords -1;
 my $mol = Molecule->new(name=> 'trp-cage', atoms=>[@atoms]);
+
+#backbone
 my @N_CA_C = grep {
                    $_->name eq 'N'  or 
                    $_->name eq 'CA' or 
                    $_->name eq 'C'   
                   } @atoms;
-my @dihedrals ;
+
+my @dihedrals ; 
 
 # abcdefgh
 my $k = 0;
@@ -25,8 +29,28 @@ while ($k+3 <= $#N_CA_C){
   $k++;
 }
 
-printf("%20s %10.2f\n", $_->name , $_->dihe) foreach @dihedrals;
+foreach my $dihe (@dihedrals){
+  #my @vals = map{$dihe->gt($_); $dihe->dihe} 0 .. $max_t;
+  printf("%20s ", $dihe->name);
+  do{$dihe->gt($_); printf("%7.2f ", $dihe->dihe) } foreach 0 .. $max_t/2;
+  print "\n";
+  # %10.2f (%.2f)\n", $dihe->name , avg_rmsd(@vals));
+}
+
 my $t2 = time;
 
 printf("time: %10.6f\n", $t2-$t1);
 
+
+sub avg {
+  my $sum = 0;
+  $sum += $_ foreach @_;
+  return ( $sum/scalar(@_) );
+}
+
+sub avg_rmsd {
+  my $avg = avg(@_);
+  my $sum = 0;
+  $sum += ($_-$avg)**2 foreach @_;
+  return ( $avg, sqrt($sum/scalar(@_)) );
+}

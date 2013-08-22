d75e1b0797c2917f05b8ff5cf2252a59b7ad644a
Demian Riccardi <dde@ornl.gov>
2013-08-22 16:51:24 -0400

    Molecule example calculates dihedrals

--- HackaMol/examples/Molecule.pl
+++ HackaMol/examples/Molecule.pl
@@ -1,21 +1,56 @@
 use Modern::Perl;
-use Math::Vector::Real;
-use lib 'lib/HackaMol';
-use Time::HiRes qw(time);
-use Benchmark qw(cmpthese);
-use Scalar::Util qw(refaddr);
-use Atom;
+use lib 'lib/HackaMol','t/lib';
 use Molecule;
+use Dihedral;
+use PDBintoAtoms qw(readinto_atoms);
+use Time::HiRes qw(time);
 
-my $natoms = 100000;
-print
-"Atom-timer will time the construction of and array of  $natoms atoms to give idea about speed\n";
+my $t1 = time; 
+#my @atoms = readinto_atoms("t/lib/2CBA.pdb");
+my @atoms = readinto_atoms("t/lib/1L2Y.pdb");
+my $max_t = $atoms[0]->count_coords -1;
+my $mol = Molecule->new(name=> 'trp-cage', atoms=>[@atoms]);
 
-my $t1 = time;
-my @atoms = map { Atom->new( Z => 80 ), coords =>[V(1,2,3)] } 1 .. $natoms;
-my $molecule = Molecule->new(name => "molecule");
+#backbone
+my @N_CA_C = grep {
+                   $_->name eq 'N'  or 
+                   $_->name eq 'CA' or 
+                   $_->name eq 'C'   
+                  } @atoms;
+
+my @dihedrals ; 
+
+# abcdefgh
+my $k = 0;
+while ($k+3 <= $#N_CA_C){
+  my $name; 
+  $name .= $_->name.$_->resid foreach (@N_CA_C[$k .. $k+3]);
+  push @dihedrals, Dihedral->new(name=>$name, atoms=>[ @N_CA_C[$k .. $k+3] ]);
+  $k++;
+}
+
+foreach my $dihe (@dihedrals){
+  #my @vals = map{$dihe->gt($_); $dihe->dihe} 0 .. $max_t;
+#  printf("%20s ", $dihe->name);
+#  do{$dihe->gt($_); printf("%7.2f ", $dihe->dihe) } foreach 0 .. $max_t/2;
+#  print "\n";
+  # %10.2f (%.2f)\n", $dihe->name , avg_rmsd(@vals));
+}
 
 my $t2 = time;
-printf( "time to Atom->new(Z => 80) for $natoms atoms: %10.3f\n", $t2 - $t1 );
 
-exit;
+printf("time: %10.6f\n", $t2-$t1);
+
+
+sub avg {
+  my $sum = 0;
+  $sum += $_ foreach @_;
+  return ( $sum/scalar(@_) );
+}
+
+sub avg_rmsd {
+  my $avg = avg(@_);
+  my $sum = 0;
+  $sum += ($_-$avg)**2 foreach @_;
+  return ( $avg, sqrt($sum/scalar(@_)) );
+}

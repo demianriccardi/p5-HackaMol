57bef1366911fd5659f7bbd0e11598b2b0bf7254
Demian Riccardi <demianriccardi@gmail.com>
2013-09-10 23:05:10 -0400

    examples need to be fixed

--- HackaMol/examples/two_molecules.pl
+++ HackaMol/examples/two_molecules.pl
@@ -1,15 +1,15 @@
 use Modern::Perl;
-use lib 'lib','t/lib';
+use lib 't/lib';
 use HackaMol::Molecule;
 use PDBintoAtoms qw(readinto_atoms);
 use Math::Vector::Real;
 
 
 my @atoms1 = readinto_atoms("t/lib/1L2Y.pdb");
-my $mol1 = Molecule->new(name=> 'trp-cage', atoms=>[@atoms1]);
+my $mol1 = HackaMol::Molecule->new(name=> 'trp-cage', atoms=>[@atoms1]);
 my @atoms2 = readinto_atoms("t/lib/2cba.pdb");
-my $mol2 = Molecule->new(name=> 'CAII', atoms=>[@atoms2]);
-my $mol3 = Molecule->new(name=> 'both', atoms=>[@atoms1,@atoms2]);
+my $mol2 = HackaMol::Molecule->new(name=> 'CAII', atoms=>[@atoms2]);
+my $mol3 = HackaMol::Molecule->new(name=> 'both', atoms=>[@atoms1,@atoms2]);
 
 $mol1->translate(-$mol1->COM);
 $mol2->translate(-$mol2->COM+V(30,0,0));

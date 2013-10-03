f8835c35877fc75d7519a9a3c37181fa5862b3aa
Demian Riccardi <dde@ornl.gov>
2013-10-03 10:02:59 -0400

    update molecule.pl example

--- examples/Molecule.pl
+++ examples/Molecule.pl
@@ -20,7 +20,7 @@ print_xyz($mol);
 
 $mol->rotate( V( 1, 0, 0 ), 180, V( 10, 10, 10 ) );
 
-print_xyz($mol);
+$mol->print_xyz;
 
 my @groups = $hack->group_by_atom_attr( 'resid', $mol->all_atoms );
 $mol->push_groups(@groups);
@@ -28,13 +28,5 @@ $mol->push_groups(@groups);
 $_->rotate( V( 1, 1, 1 ), 60, $_->COM, 1 ) foreach $mol->all_groups;
 
 $mol->gt(1);
-print_xyz($mol);
+$mol->print_xyz;
 
-sub print_xyz {
-    my $mol = shift;
-    print $mol->count_atoms;
-    print "\n\n";
-    foreach my $atom ( $mol->all_atoms ) {
-        printf( "%5s %12.6f %12.6f %12.6f\n", $atom->Z, @{ $atom->xyz } );
-    }
-}

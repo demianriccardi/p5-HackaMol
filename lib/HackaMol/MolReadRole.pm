a4b8490d2056f3918f1035da0d12ae1827153091
Demian Riccardi <dde@ornl.gov>
2014-04-21 10:37:44 -0400

    update POD

--- lib/HackaMol/MolReadRole.pm
+++ lib/HackaMol/MolReadRole.pm
@@ -340,13 +340,19 @@ __END__
 
 =head1 SYNOPSIS
 
-use HackaMol;
+   use HackaMol;
 
-my $hack   = HackaMol->new( name => "hackitup" );
-my @atoms1 = $hack->read_file_atoms("t/lib/1L2Y.pdb"); 
-my @atoms2 = $hack->read_file_atoms("t/lib/something.xyz"); 
-my $mol    = HackaMol->new( name => "merger", atoms => [@atoms1,@atoms2]);
-$mol->print_pdb;  
+   my $hack   = HackaMol->new( name => "hackitup" );
+
+   # build array of carbon atoms from pdb [xyz,pdbqt] file
+   my @carbons  = grep {$_->symbol eq "C"} $hack->read_file_atoms("t/lib/1L2Y.pdb"); 
+   my $Cmol     = HackaMol::Molecule->new(name => "carbonprotein", atoms => [ @carbons ]);
+   $Cmol->print_pdb;   
+   $Cmol->print_xyz;     
+
+   # build molecule from xyz [pdb,pdbqt] file
+   my $mol    = $hack->read_file_mol("some.xyz");
+   $mol->print_pdb; # with junk attributes... not trivial to add information going from xyz to pdb 
 
 =head1 DESCRIPTION
 

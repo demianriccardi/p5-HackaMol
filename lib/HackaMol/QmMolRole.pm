2f889ebe7e51b58e9e0163051bf1f8bd7bed02a8
Demian Riccardi <demianriccardi@gmail.com>
2014-05-04 20:34:24 -0400

    added score and energy attributes
    pod and tests needed... QmMolRole needs to be completely reworked

--- lib/HackaMol/QmMolRole.pm
+++ lib/HackaMol/QmMolRole.pm
@@ -9,8 +9,8 @@ with 'HackaMol::QmAtomRole';
 has 'multiplicity', is => 'rw', isa => 'Int', lazy => 1, default => 1;
 
 my @tscl = qw(
-  Score
-  Energy
+  score
+  energy
   Etot Eelec Enuc
   qm_dipole_moment ionization_energy gradient_norm
   Hform

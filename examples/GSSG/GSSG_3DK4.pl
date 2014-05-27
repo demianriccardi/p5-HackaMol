becdf4e644cd4a688f6aa024519965d128c265a5
Demian Riccardi <dde@ornl.gov>
2014-05-27 16:22:31 -0400

    GSSG examples to generate/modify ox glutathione

---
+++ examples/GSSG/GSSG_3DK4.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+# This script will: 
+#  1. pull down pdbid 3DK4 from the PDB
+#  2. extract the GSSG
+#  3. write out a new pdb containing only the GSSG 
+#
+# pubchem id can also be used to fetch
+# @ids = qw/65359/;
+#
+use Modern::Perl;
+use HackaMol;
+
+my $hack = HackaMol->new(scratch=>'structures');
+$hack->scratch->mkpath unless $hack->scratch->exists;
+
+unless ( -e "structures/3DK4.pdb" ){
+  system("wget http://pdb.org/pdb/files/3DK4.pdb");
+  system("mv 3DK4.pdb structures/3DK4.pdb");
+} 
+
+
+my @atoms = $hack->read_file_atoms("structures/3DK4.pdb");
+
+my $hetmol = HackaMol::Molecule->new(
+    atoms => [
+        grep { $_->resname eq 'GSH' } @atoms,
+    ]
+);
+
+$hetmol->print_pdb("structures/GSSG.pdb");
+

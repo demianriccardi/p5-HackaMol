700a995b6d49f1cdc2662f3982485ccb5dff4d71
Demian Riccardi <demianriccardi@gmail.com>
2014-07-25 13:34:41 -0400

    bump version

--- examples/pdbqt_pdb.pl
+++ examples/pdbqt_pdb.pl
@@ -2,8 +2,8 @@
 use Modern::Perl;
 use HackaMol;
 
-my $hack = HackaMol->new( name => "hackitup" );
-my @atoms = $hack->read_file_atoms("t/lib/test.pdbqt");
+my $bldr = HackaMol->new( name => "builder", hush_read=>1 );
+my @atoms = $bldr->read_file_atoms("t/lib/test.pdbqt");
 
 # all coordinates from NMR ensemble are loaded into atoms
 my $mol = HackaMol::Molecule->new(

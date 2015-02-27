6f181690b698194e4e6a0ac84efae7406a3a2cce
demianriccardi <demianriccardi@gmail.com>
2015-02-26 21:29:49 -0500

    script for generating UML of HackaMol

---
+++ examples/Wonkish/UML.pl
@@ -0,0 +1,13 @@
+use Modern::Perl;
+use UML::Class::Simple;
+use Data::Dumper;
+
+my @classes = classes_from_files(['lib/HackaMol.pm',glob('lib/HackaMol/*.pm')],'/Role/');
+my $painter = UML::Class::Simple->new(\@classes);
+$painter->inherited_methods(0);
+
+$painter->as_png('shit.png');
+
+#print Dumper $painter->as_dom();
+
+

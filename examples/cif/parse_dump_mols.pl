bb43aa3d5a058751f5749e14af20503cb8373d26
demianriccardi <demianriccardi@gmail.com>
2018-09-06 21:24:14 -0600

    quick example

---
+++ examples/cif/parse_dump_mols.pl
@@ -0,0 +1,25 @@
+use Modern::Perl;
+use HackaMol;
+use Time::HiRes qw(time);
+use Data::Dumper;
+
+my $file = shift;
+
+open my $fh, "<", $file or die 'cant open file';
+
+my $bldr = HackaMol->new();
+my $t1 = time;
+my ($info, $mols) = $bldr->read_file_cif_parts($file);
+my $t2 = time;
+
+print Dumper $info;
+say scalar(@$mols);
+printf ("parse time: %10.6f\n", $t2 - $t1);
+
+open my $fh2, ">", 'shit.xyz';
+
+foreach my $mol (@$mols){
+    $mol->print_xyz($fh2);
+}
+
+exit;
